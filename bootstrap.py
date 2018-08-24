ab57258a6cb93d814a80f4aabd4916b21639639c
Ryan Bradley <bradleyrp@gmail.com>
2018-08-23 22:21:01 -0400

    added files

---
+++ bootstrap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+"""
+BOOTSTRAP the FACTORY configuration
+"""
+
+# default configuration is written to config.json on first make
+default_configuration = {
+	'commands':['manager/factory.py','ortho/replicator/docker.py'],
+	'docks_config':'ortho/replicator/docker_config.py',
+	'user_creds':'Bond998*',
+	'automacs':{'address':'http://github.com/biophyscode/automacs','branch':'ortho'},
+	'omnicalc':{'address':'http://github.com/biophyscode/omnicalc'},
+	'docs':{'list':{
+		'factory':{'source':'manager/docs_source','build':'docs_factory'},
+		'ortho':{'source':'ortho/docs_source','build':'docs_ortho'}}}}
+
+def bootstrap_default(): return default_configuration
+def bootstrap_post(): return
