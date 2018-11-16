75696dd0e69490ccaa97a1effc5a68e02f2b0527
Ryan Bradley <bradleyrp@gmail.com>
2018-11-16 08:23:26 -0500

    Squashed 'ortho/' changes from 86cd191..0a821c1
    0a821c1 merged
    d6a331c fixed regex error in print emphasis
    7ce585d casting as string in overloaded print
    5e686fc merged ortho on factory
    fd35f39 minor development on factory
    
    git-subtree-dir: ortho
    git-subtree-split: 0a821c1a5850213f711d0d4c671e8a1da982527b

--- __init__.py
+++ __init__.py
@@ -78,7 +78,7 @@ def prepare_print(override=False):
 		key_leads = ['status','warning','error','note','usage',
 			'exception','except','question','run','tail','watch',
 			'bash','debug']
-		key_leads_regex = re.compile(r'^(%s)\s*(.+)$'%'|'.join(key_leads))
+		key_leads_regex = re.compile(r'^(?:(%s)\s)(.+)$'%'|'.join(key_leads))
 		def print_stylized(*args,**kwargs):
 			"""Custom print function."""
 			if len(args)>0 and args[0] in key_leads:
@@ -87,7 +87,7 @@ def prepare_print(override=False):
 			#   statements to be capitalized
 			#! note that we can retire all print('debug','message') statements
 			elif len(args)==1:
-				match = key_leads_regex.match(args[0])
+				match = key_leads_regex.match(str(args[0]))
 				if match: return _print(
 					'[%s]'%match.group(1).upper(),match.group(2),**kwargs)
 				else: return _print(*args,**kwargs)
@@ -140,9 +140,7 @@ for mod,ups in expose.items():
 	# note the utility functions for screening later
 	globals()[mod].__dict__['_ortho_keys'] = _ortho_keys
 	for up in ups: 
-		try: globals()[up] = globals()[mod].__dict__[up]
-		except:
-			import pdb;pdb.set_trace()
+		globals()[up] = globals()[mod].__dict__[up]
 
 # if the tee flag is set then we dump stdout and stderr to a file
 tee_fn = conf.get('tee',False)
