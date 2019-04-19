52cc9633b6a050151506935fc1caeffe79757a19
Ryan Bradley <bradleyrp@gmail.com>
2019-04-19 19:00:39 -0400

    testing replicator with some fixes

--- ortho/replicator/replicator.py
+++ ortho/replicator/replicator.py
@@ -3,6 +3,7 @@
 import re,glob
 
 __all__ = ['repl','pipeline','test_clean','docker_clean','get_jupyter_token']
+#! the all above does not filter Handler and Site out of the makefile interface
 
 import ortho
 from ortho import str_types
@@ -26,15 +27,19 @@ def replicator_read_yaml(sources,name=None,args=None,kwargs=None):
 	"""
 	Read a replicator instruction and send it to the Guide for execution.
 	"""
+	if args or kwargs: raise Exception((
+		'not sure what to do with these: args=%s, kwargs=%s'%(
+			str(args),kwargs)))
 	import yaml
-
 	incoming = {}
 	for source in sources:
 		with open(source) as fp: 
 			this = yaml.load(fp.read())
 			for key,val in this.items():
 				if key in incoming: 
-					raise Exception('duplicate keys in replicator YAML files (%s): %s'%(source,key))
+					raise Exception(
+						'duplicate keys in replicator YAML files (%s): %s'%(
+							source,key))
 				incoming[key] = val
 	# we load into a MultiDict to forbid spaces (replaced with 
 	#   underscores) in top-level dictionary.
@@ -43,7 +48,7 @@ def replicator_read_yaml(sources,name=None,args=None,kwargs=None):
 	# special handling
 	reference = {}
 	# previously used custom taxonomy but here we infer it via inspect
-	taxonomy_rs = ReplicatorSpecial(inspect=True).taxonomy
+	taxonomy_rs = ReplicatorSpecial(inspect=True)._taxonomy
 	for key in taxonomy_rs:
 		if key in instruct: 
 			reference[key] = ReplicatorSpecial(name=key,
@@ -106,7 +111,8 @@ def repl(*args,**kwargs):
 	# instructions for extending ReplicatorGuide
 	# see hooks/replicator_alt.py
 	# EXTEND the ReplicatorGuide class
-	# make set_hook replicator="\"{'s':'hooks/replicator_alt.py','f':'update_replicator_guide'}\""
+	# make set_hook replicator="\"{'s':'hooks/replicator_alt.py',
+	#   'f':'update_replicator_guide'}\""
 	# import the class, subclass it, and export that inside a function
 	from ortho.replicator.formula import ReplicatorGuide
 	class ReplicatorGuide(ReplicatorGuide):
@@ -120,6 +126,16 @@ def repl(*args,**kwargs):
 	# +++ allow user to hook in other handlers here
 	import ortho
 	ortho.hooks.hook_merge(hook='replicator',namespace=globals())
+	# some special flags from the terminal
+	rebuild = False
+	if 'rebuild' in args:
+		print('status received the `rebuild` flag from terminal')
+		args = tuple([i for i in args if i!='rebuild'])
+		rebuild = True
+	cname = None
+	if 'cname' in kwargs: 
+		#! this feature does not work with docker exec for some reason
+		cname = kwargs.pop('cname')
 	# allow args to be handled by the interface key for easier CLI
 	if args:
 		if kwargs: raise Exception('cannot use kwargs with args here')
@@ -136,6 +152,15 @@ def repl(*args,**kwargs):
 		kwargs['sources'] = [kwargs.pop('source')]
 		this_test = replicator_read_yaml(**kwargs)
 	else: raise Exception('unclear request')
+	# handle special arguments
+	if rebuild and 'rebuild' not in this_test['detail']:
+		raise Exception(('cannot pass rebuild to ReplicatorGuide '
+			'because this experiment lacks it: %s'%this_test['name']))
+	elif rebuild: this_test['detail']['rebuild'] = True
+	# container name is passed on and will cause an error in the
+	#   ReplicatorGuide if it cannot be handled, however the docker_compose 
+	#   recipe dynamically adds it to the compose file for this container
+	if cname: this_test['detail']['cname'] = cname
 	# run the replicator
 	rg = ReplicatorGuide(name=this_test['name'],
 		meta=this_test['meta'],**this_test['detail'])
