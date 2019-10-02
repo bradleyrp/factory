11dd3d89641bddf86b8a4a28a32637fe4140837a
Ryan Bradley <bradleyrp@gmail.com>
2019-10-01 20:39:14 -0400

    link documentation to factory env

--- ortho/documentation.py
+++ ortho/documentation.py
@@ -3,13 +3,18 @@
 from __future__ import print_function
 from .bash import bash
 from .config import read_config
+from .requires import is_terminal_command
 
 import os
 
-def build_docs(source='ortho/docs_source',build='docs',single=False	):
+def build_docs(source='ortho/docs_source',build='docs',single=False):
 	"""Make documentation."""
 	if build=='build_docs': 
 		raise Exception('name collision. you cannot build docs in the name of this function: %s'%build)
+	# if no sphinx-build and we see a factory interface ("fac") we use it
+	via_factory = (
+		os.path.isfile('fac') and 
+		is_terminal_command('sphinx-build')!=0)
 	docs_default = {'local':{'source':source,'build':build}}
 	conf = read_config()
 	if conf.get('docs',{}).get('list',None): 
@@ -22,7 +27,10 @@ def build_docs(source='ortho/docs_source',build='docs',single=False	):
 	urls = {}
 	for name,detail in docs.items():
 		source,build = detail['source'],detail['build']
-		bash('sphinx-build -b html %s %s%s'%(source,build,' '+' '.join(opts) if opts else ''),cwd='.')
+		cmd = 'sphinx-build -b html %s %s%s'%(
+			source,build,' '+' '.join(opts) if opts else '')
+		if via_factory: cmd = './fac envrun '+cmd
+		bash(cmd,cwd='.',announce=True)
 		index = os.path.abspath(os.path.join(build,'index.html'))
 		if not os.path.isfile(index): raise Exception('building docs "%s" failed'%name)
 		urls[name] = index
