f42ed1e9ea6aface4b116420f553973bbbe177b2
Ryan Bradley <bradleyrp@gmail.com>
2019-10-17 12:51:25 -0400

    source the environment from any directory

--- env.sh
+++ env.sh
@@ -3,7 +3,9 @@
 if [ -z $1 ]; then
 	./fac envs
 else
-	cmd="source conda/bin/activate conda/envs/$1"
+	# identify the parent directory via https://stackoverflow.com/a/246128/3313859
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+	cmd="source $DIR/conda/bin/activate $DIR/conda/envs/$1"
 	echo "[STATUS] running: $cmd"
 	eval $cmd
 fi
