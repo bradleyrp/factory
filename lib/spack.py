98f286757b21fd0a0d932ccc704710591787271d
Ryan Bradley <bradleyrp@gmail.com>
2019-11-07 07:31:11 -0500

    adding spack_env subcommand

--- lib/spack.py
+++ lib/spack.py
@@ -4,6 +4,7 @@ import os,copy
 import ortho
 import yaml
 from lib.yaml_mods import YAMLObjectInit
+from ortho import Handler
 
 class OrthoSync(YAMLObjectInit):
 	"""Trivial wrapper around ortho sync."""
@@ -75,3 +76,41 @@ class SpackTree(yaml.YAMLObject):
 		self.spot = spot
 		if not os.path.isdir(self.spot):
 			os.mkdir(self.env)
+
+#!!! redeveloping here
+
+def spack_clone(where='local'):
+	"""
+	Clone a copy of spack for local use.
+	"""
+	os.makedirs(where,exist_ok=True)
+	if where.split(os.path.sep)[-1]=='spack':
+		raise Exception('invalid path cannot end in "spack": %s'%where)
+	ortho.bash('git clone https://github.com/spack/spack',cwd=where)
+	#! should we have a central conf registrar?
+	ortho.conf['spack'] = os.path.realpath(os.path.join(where,'spack'))
+	ortho.write_config()
+
+class SpackEnvMaker(Handler):
+	def _run_via_spack(self,spack_spot,env_spot,command):
+		starter = os.path.join(spack_spot,'share/spack/setup-env.sh')
+		#! replace this with a pointer like the ./fac pointer to conda?
+		ortho.bash('source %s && %s'%
+			(starter,command),cwd=env_spot)
+	def std(self,spack,where,spack_spot):
+		os.makedirs(where,exist_ok=True)
+		with open(os.path.join(where,'spack.yaml'),'w') as fp:
+			yaml.dump({'spack':spack},fp)
+		self._run_via_spack(spack_spot=spack_spot,env_spot=where,
+			command='spack concretize -f')
+
+def spack_env_maker(what):
+	"""
+	Install a spack environment.
+	"""
+	spack = ortho.conf.get('spack',None)
+	if not spack: spack_clone()
+	with open(what) as fp: 
+		instruct = yaml.load(fp,Loader=yaml.SafeLoader)
+	print(instruct)
+	SpackEnvMaker(spack_spot=spack,**instruct).solve
