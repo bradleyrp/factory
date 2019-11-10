3fdef1159a53c545e531f23ad5d5ebc401c19162
Ryan Bradley <bradleyrp@gmail.com>
2019-11-09 19:51:46 -0500

    moved ortho sync interface to generic

---
+++ lib/generic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from lib.yaml_mods import YAMLObjectInit
+
+class OrthoSync(YAMLObjectInit):
+	"""Trivial wrapper around ortho sync."""
+	yaml_tag = '!ortho_sync'
+	def __init__(self,**kwargs):
+		self.sources = kwargs
+		if kwargs.get('until',False): return
+		else: self._run()
+	def _run(self):
+		# reformulate the modules
+		kwargs_out = {}
+		for key in self.sources:
+			spot = self.sources[key]['spot']
+			if spot in kwargs_out:
+				raise Exception('repeated key: %s'%spot)
+			kwargs_out[spot] = dict([(i,j) 
+				for i,j in self.sources[key].items()
+				if i!='spot'])
+		ortho.sync(modules=kwargs_out)
