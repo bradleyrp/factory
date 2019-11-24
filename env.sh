7a6cfec046f9d37f70b9fe94059345b1f66b2858
Ryan Bradley <bradleyrp@gmail.com>
2019-11-24 14:25:21 -0500

    error if the script is not sourced

--- env.sh
+++ env.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
-# do not exit a source command or it causes you to leave screen
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if [ -z "$1" ]; then 
-	make envs
-	echo "[USAGE] see the list above for environments"
-	echo "[USAGE] use spack: source env.sh spack"
-	echo "[USAGE] use an environment: source env.sh <name>"
+if [[ ! $_ == $0 ]]; then
+	echo "[ERROR] you must source env.sh"
+	echo "[ERROR] source env.sh without arguments for usage notes"
+# note that we nest all conditionals because exit in a sourced script
+# causes you to leave a screen which is very annoying
 else
-	# special name for sourcing spack
-	if [ "$1" == "spack" ]; then name=_spack
-	else name=$1; fi
-	# confirm that the environment exists
-	make -C $DIR envs name=$name > /dev/null 2>&1
-	if [ ! $? -eq 0 ]; then
-		# repeat the command to show the error
-		make -C $DIR envs name=$name
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+	if [ -z "$1" ]; then 
+		make envs
+		echo "[USAGE] see the list above for environments"
+		echo "[USAGE] use spack: source env.sh spack"
+		echo "[USAGE] use an environment: source env.sh <name>"
 	else
-		#! could we detect a lack of source command because otherwise pointless?
-		# source  the environment
-		# remote directory via https://stackoverflow.com/a/246128/3313859
-		read -a paths <<<$(make -C $DIR envs name=$name \
-			| perl -ne 'print if s/source (.+)$/$1/')
-		# source the correct environment
-		if [ "$name" == "_spack" ]; then
-			echo "[STATUS] sourcing spack"
-			# the call to get the spack environment yields the spack setup directly
-			source $paths
-		else	
-			echo "[STATUS] sourcing the \"$name\" environment"
-			echo "[STATUS] use \"conda deactivate\" to exit"
-			# requesting an environment gives a relative path
-			resolved="$DIR/${paths[0]} $DIR/${paths[1]}"
-			source $resolved
+		# special name for sourcing spack
+		if [ "$1" == "spack" ]; then name=_spack
+		else name=$1; fi
+		# confirm that the environment exists
+		make -C $DIR envs name=$name > /dev/null 2>&1
+		if [ ! $? -eq 0 ]; then
+			# repeat the command to show the error
+			make -C $DIR envs name=$name
+		# source the right environment
+		else
+			# remote directory via https://stackoverflow.com/a/246128/3313859
+			read -a paths <<<$(make -C $DIR envs name=$name \
+				| perl -ne 'print if s/source (.+)$/$1/')
+			# source the correct environment
+			if [ "$name" == "_spack" ]; then
+				echo "[STATUS] sourcing spack"
+				# the call to get the spack environment yields the spack setup
+				source $paths
+			else	
+				echo "[STATUS] sourcing the \"$name\" environment"
+				echo "[STATUS] use \"conda deactivate\" to exit"
+				# requesting an environment gives a relative path
+				resolved="$DIR/${paths[0]} $DIR/${paths[1]}"
+				source $resolved
+			fi
 		fi
 	fi
 fi
