945f57d247a285daffbd2f8ddc3d540c677cf724
Ryan Bradley <bradleyrp@gmail.com>
2019-12-11 21:35:28 -0500

    zsh compatibility

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	echo "[ERROR] you must source env.sh"
 	echo "[ERROR] source env.sh without arguments for usage notes"
@@ -13,7 +13,8 @@ else
 		echo "[USAGE] use an environment: source env.sh <name>"
 	else
 		# special name for sourcing spack
-		if [ "$1" == "spack" ]; then name=_spack
+		# not zsh compatible: if [ "$1" == "spack" ]; then name=_spack
+		if [ "$1" = "spack" ]; then name=_spack
 		else name=$1; fi
 		# confirm that the environment exists
 		make -C $DIR envs name=$name > /dev/null 2>&1
@@ -23,19 +24,27 @@ else
 		# source the right environment
 		else
 			# remote directory via https://stackoverflow.com/a/246128/3313859
-			read -a paths <<<$(make -C $DIR envs name=$name \
+			if [ "$SHELL" = "/bin/zsh" ]; then read_flag="-A"
+			else read_flag="-a"; fi
+			read $read_flag paths <<<$(make -C $DIR envs name=$name \
 				| perl -ne 'print if s/source (.+)$/$1/')
 			# source the correct environment
-			if [ "$name" == "_spack" ]; then
+			if [ "$name" = "_spack" ]; then
 				echo "[STATUS] sourcing spack"
 				# the call to get the spack environment yields the spack setup
 				source $paths
 			else	
 				echo "[STATUS] sourcing the \"${name}\" environment"
 				echo "[STATUS] use \"conda deactivate\" to exit"
-				# requesting an environment gives a relative path
-				resolved="$DIR/${paths[0]} $DIR/${paths[1]}"
-				source $resolved
+				if [ "$SHELL" = "/bin/zsh" ]; then 
+					# requesting an environment gives a relative path
+					source "$DIR/${paths[1]}" "$DIR/${paths[2]}"
+					 $resolved
+				else
+					# requesting an environment gives a relative path
+					resolved="$DIR/${paths[0]} $DIR/${paths[1]}"
+					source $resolved
+				fi
 			fi
 		fi
 	fi
