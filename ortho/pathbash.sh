4c26c07ebf264a91233ab2157a1c594240c10330
Ryan Bradley <bradleyrp@gmail.com>
2020-10-10 09:58:31 -0400

    added path manipulations without repeats

---
+++ ortho/pathbash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# PATH manipulation in BASH
+# via https://stackoverflow.com/a/24522107
+# USAGE: field_prepend varName fieldVal [sep]
+field_prepend() {
+    local varName=$1 fieldVal=$2 IFS=${3:-':'} auxArr
+    read -ra auxArr <<< "${!varName}"
+    for i in "${!auxArr[@]}"; do
+        [[ ${auxArr[i]} == "$fieldVal" ]] && unset auxArr[i]
+    done
+    auxArr=("$fieldVal" "${auxArr[@]}")
+    printf -v "$varName" '%s' "${auxArr[*]}"
+}
+# USAGE: field_append varName fieldVal [sep]
+field_append() {
+    local varName=$1 fieldVal=$2 IFS=${3:-':'} auxArr
+    read -ra auxArr <<< "${!varName}"
+    for i in "${!auxArr[@]}"; do
+        [[ ${auxArr[i]} == "$fieldVal" ]] && unset auxArr[i]
+    done
+    auxArr+=("$fieldVal")
+    printf -v "$varName" '%s' "${auxArr[*]}"
+}
+# USAGE: field_remove varName fieldVal [sep]
+field_remove() {
+    local varName=$1 fieldVal=$2 IFS=${3:-':'} auxArr
+    read -ra auxArr <<< "${!varName}"
+    for i in "${!auxArr[@]}"; do
+        [[ ${auxArr[i]} == "$fieldVal" ]] && unset auxArr[i]
+    done
+    printf -v "$varName" '%s' "${auxArr[*]}"
+}
